bb0ebabf72e9682fcdb40f574222899a1bc50524
Spencer Billings <Sbillings6302@conestogac.on.ca>
2020-05-16 07:56:49 -0400

    Replaced ConsolePrinter class with Console.WriteLine calls, and reworked main program flow.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,101 +6,91 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace ConsoleApp1
-{
-    class Program
-    {
+namespace ConsoleApp1 {
+    class Program {
         //results does not need to be global, make this private/parameter
         static string[] results = new string[50];
         static Tuple<string, string> names;
-        static ConsolePrinter printer = new ConsolePrinter();
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             char key;
 
-            printer.Value("Press ? to get instructions.").ToString();
-            //If user doesn't enter '?' program exits. Remove this since only workflow is to get instructions
-            if (Console.ReadLine() == "?")
-            {
-                //Give user option to exit program, loop until exit parameters are fulfilled
-                while (true)
-                {
-                    printer.Value("Press c to get categories").ToString();
-                    printer.Value("Press r to get random jokes").ToString();
-                    key = GetEnteredKey();
-                    if (key == 'c')
-                    {
-                        //Get category list, and display it to user
-                        getCategories();
+            //Give user option to exit program, loop until exit parameters are fulfilled
+            do {
+                //Clean up the console window from any previous loops.
+                Console.Clear();
+                //Display Main Menu Options to user.
+                Console.WriteLine("Press c to get categories");
+                Console.WriteLine("Press r to get random jokes");
+                Console.WriteLine("Press x to exit");
+                key = GetEnteredKey();
+                if (key == 'c') {
+                    //Get category list, and display it to user
+                    getCategories();
+                    PrintResults();
+                } else if (key == 'r') {
+                    //Generate 1-9 random jokes for the user
+                    Console.WriteLine("Want to use a random name? y/n");
+
+                    if (GetEnteredKey() == 'y') {
+                        //Regenerate random name list every time
+                        GetNames();
+                    }
+                    Console.WriteLine("Want to specify a category? y/n");
+                    if (GetEnteredKey() == 'y') {
+                        //Remove duplication of code
+                        Console.WriteLine("How many jokes do you want? (1-9)");
+                        int n = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter a category;");
+                        GetRandomJokes(Console.ReadLine(), n);
+                        PrintResults();
+                    } else {
+                        Console.WriteLine("How many jokes do you want? (1-9)");
+                        int n = Int32.Parse(Console.ReadLine());
+                        GetRandomJokes(null, n);
                         PrintResults();
                     }
-                    if (key == 'r')
-                    {
-                        //Generate 1-9 random jokes for the user
-                        printer.Value("Want to use a random name? y/n").ToString();
-                        
-                        if (GetEnteredKey() == 'y') {
-                            //Regenerate random name list every time
-                            GetNames();
-                        }
-                        printer.Value("Want to specify a category? y/n").ToString();
-                        if (GetEnteredKey() == 'y')
-                        {
-                            //Remove duplication of code
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-                            printer.Value("Enter a category;").ToString();
-                            GetRandomJokes(Console.ReadLine(), n);
-                            PrintResults();
-                        }
-                        else
-                        {
-                            printer.Value("How many jokes do you want? (1-9)").ToString();
-                            int n = Int32.Parse(Console.ReadLine());
-                            GetRandomJokes(null, n);
-                            PrintResults();
-                        }
-                    }
-                    names = null;
+                } else if (key == 'x') {
+                    //Exit the program cleanly.
+                    System.Environment.Exit(0);
                 }
-            }
+                Console.WriteLine("Press Any Key to Start Again..");
+                Console.ReadKey();
+
+                names = null;
+            } while (key != 'x');
+
 
         }
 
 
-        private static void PrintResults()
-        {
-            printer.Value("[" + string.Join(",", results) + "]").ToString();
+        private static void PrintResults() {
+            Console.WriteLine("[" + string.Join(",", results) + "]");
         }
 
         /// <summary>
         /// Waits for the user to enter a key, then converts it into the lower case value.
         /// </summary>
         /// <returns>Returns the key pressed as lowercase invariant</returns>
-        private static char GetEnteredKey()
-        {
-            
+        private static char GetEnteredKey() {
+
             char key = Char.ToLowerInvariant(Console.ReadKey().KeyChar);
             return key;
         }
 
-        private static void GetRandomJokes(string category, int number)
-        {
+        private static void GetRandomJokes(string category, int number) {
             new JsonFeed("https://api.chucknorris.io", number);
             //Loop the random joke generator for the number of jokes requested.
             results = JsonFeed.GetRandomJokes(names?.Item1, names?.Item2, category);
         }
 
-        private static void getCategories()
-        {
+        private static void getCategories() {
             //Correct endpoint for this is: https://api.chucknorris.io/jokes/categories
             new JsonFeed("https://api.chucknorris.io", 0);
             results = JsonFeed.GetCategories();
         }
 
-        private static void GetNames()
-        {
+        private static void GetNames() {
             //Update uri to include query string ?amount= (allows up to 500) to allow each joke to have random name
             new JsonFeed("https://names.privserv.com/api/", 0);
             dynamic result = JsonFeed.Getnames();
